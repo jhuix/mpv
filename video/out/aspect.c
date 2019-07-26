ecf0f03bb76e89eaf170bbec6c1ca952ed641b2b
Hui Jin <jhuix0117@gmail.com>
2019-07-26 15:01:29 +0800

    vo/aspect: additional function of "no-keepaspect-window" parameter The additional function of "no-keepaspect-window" parameter: scale to the whole window while keeping aspect ratio.

--- video/out/aspect.c
+++ video/out/aspect.c
@@ -128,6 +128,16 @@ void mp_get_src_dst_rects(struct mp_log *log, struct mp_vo_opts *opts,
         MPSWAP(int, src_w, src_h);
         MPSWAP(int, src_dw, src_dh);
     }
+    // Additional function of "no-keepaspect-window" parameter,
+    // Scale to the whole window while keeping aspect ratio.
+    if (opts->keepaspect && !opts->keepaspect_window) {
+        double wscale = (double)window_w / (double)src_dw;
+        double hscale = (double)window_h / (double)src_dh;
+        if (wscale > hscale)
+            window_h = src_dh * wscale;
+        else if (wscale < hscale)
+            window_w = src_dw * hscale;
+    }
     window_w = MPMAX(1, window_w);
     window_h = MPMAX(1, window_h);
     struct mp_rect dst = {0, 0, window_w, window_h};
